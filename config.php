8c0503ea9f2577373dcdbadc90b6d8c2b9e452a0
josecarlosA4 <105609869+josecarlosA4@users.noreply.github.com>
2022-07-29 16:00:44 -0300

    Update config.php

--- config.php
+++ config.php
@@ -4,8 +4,8 @@ session_start();
 
 $base = 'http://localhost/projetos_php/myBlogsOO';
 $db_name = 'myblogs';
-$db_host = 'localhost';
-$db_user = 'root';
+$db_host = '';
+$db_user = '';
 $db_pass = '';
 
 $pdo = new PDO("mysql:dbname=".$db_name.";host=".$db_host,$db_user,$db_pass);
